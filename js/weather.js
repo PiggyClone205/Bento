dcef005614c348204f3053d064e98857f4913a55
PiggyClone205 <33158029+PiggyClone205@users.noreply.github.com>
2021-04-29 12:29:37 +0000

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = 'cba2f0ec8ba1ef1888382d0eeb74b0b9';
 
 // Set Position function
 setPosition();
