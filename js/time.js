be32f45bd454c25b41dfa27d9a4ade3680f74f97
PiggyClone205 <33158029+PiggyClone205@users.noreply.github.com>
2021-05-01 03:21:13 +0000

    Update time.js

--- js/time.js
+++ js/time.js
@@ -18,7 +18,8 @@ function displayClock() {
   // Set to true to use a 12 hour date format
   var format_12hour = true;
 
-  var d = new Date(+date + 'z');
+  var d = new Date();
+  d = new Date(+d + d.getTimezoneOffset() * 6e4);
   var mm = monthNames[d.getMonth()];
   var dd = d.getDate();
   var min = (mins = ('0' + d.getMinutes()).slice(-2));
